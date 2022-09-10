e8d1efb278ae70078560d7cd3b6762b8acf9fdb7
Kayla Bonneteau <k.bonneteau@gmail.com>
2022-09-09 22:19:32 -0700

    feature/webpack add dev server option for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,13 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -14,6 +19,10 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: "/",
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
   module: {
     rules: [
