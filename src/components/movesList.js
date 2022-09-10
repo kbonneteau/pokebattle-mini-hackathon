9332e0f61d923960309b14a37cd7db2027902785
Kayla Bonneteau <k.bonneteau@gmail.com>
2022-09-10 15:05:26 -0700

    wip/refactor add attack button to player dialog

--- src/components/movesList.js
+++ src/components/movesList.js
@@ -4,12 +4,16 @@ const MovesList = () => {
   const moves = createNewElement("form", ["player-attacks"]);
   const interactionLabel = createNewElement("label", [], "move");
   const movesList = createNewElement("select");
+  const attackBtn = createNewElement("button", ["submit"], "Attack!");
+
+  attackBtn.setAttribute("type", "submit");
   interactionLabel.setAttribute("for", "move");
   movesList.setAttribute("name", "move");
   movesList.id = "player-moves";
 
   moves.appendChild(interactionLabel);
   moves.appendChild(movesList);
+  moves.appendChild(attackBtn);
   return moves;
 };
 
