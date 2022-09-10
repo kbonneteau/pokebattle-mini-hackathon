97dd26cd65f94d1a15d38dd42c573f02bf2952e7
Kayla Bonneteau <k.bonneteau@gmail.com>
2022-09-10 12:27:36 -0700

    wip/refactor add template service from building html document nodes

---
+++ src/services/templateFunctions.js
@@ -0,0 +1,32 @@
+/**
+ * Creates a new document node for appending to current document
+ * @param {string} type
+ * @param {array} classNames
+ * @param {string} text
+ */
+export const createNewElement = (type, classNames = [], text = "") => {
+  const allowedTags = [
+    "header",
+    "footer",
+    "div",
+    "section",
+    "p",
+    "span",
+    "ul",
+    "ol",
+    "li",
+    "img",
+  ];
+
+  if (!allowedTags.includes(type)) {
+    throw new Error(`${type} is not an allowed tag`);
+  }
+  if (!Array.isArray(classNames)) {
+    throw new TypeError("arg classNames must be an array");
+  }
+
+  const newNode = document.createElement(type);
+  classNames.forEach((className) => newNode.classList.add(className));
+  newNode.innerText = text;
+  return newNode;
+};
